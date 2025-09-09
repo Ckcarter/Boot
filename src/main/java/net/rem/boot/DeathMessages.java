047a297d30cf694c7de21551281d2b3f5c6e81be
Ché Carter <Ch@Home.com>
2025-09-09 17:51:34 -0500

    Fixed death text 09/09/25

--- src/main/java/net/rem/boot/DeathMessages.java
+++ src/main/java/net/rem/boot/DeathMessages.java
@@ -15,18 +15,18 @@ public class DeathMessages {
             DamageSource source = event.getSource();
             if (source.is(BootMod.BOOT_DAMAGE_TYPE)) {
                 Component message;
-                if (source.getEntity() instanceof Player killer) {
-                    message = Component.translatable(
-                            "death.attack.bootmod.boot.player",
-                            player.getDisplayName(),
-                            killer.getDisplayName());
-                } else {
-                    message = Component.translatable(
-                            "death.attack.bootmod.boot",
-                            player.getDisplayName());
+                Player killer = null;
+                if (source.getEntity() instanceof Player p) {
+                    killer = p;
+                } else if (source.getEntity() instanceof net.rem.boot.entity.custom.BootEntity boot && boot.getOwner() instanceof Player p) {
+                    killer = p;
                 }
-            }
 
+                Component message = killer != null
+                        ? Component.translatable("death.attack.bootmod.boot.player", player.getDisplayName(), killer.getDisplayName())
+                        : Component.translatable("death.attack.bootmod.boot", player.getDisplayName());
+                event.setDeathMessage(message);
+            }
         }
     }
 }
