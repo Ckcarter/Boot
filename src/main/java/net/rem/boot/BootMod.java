b8496581caaa7a428f62adc31b71fef90d117377
Che Carter <checarter80@gmail.com>
2025-09-09 10:53:48 -0500

    Testing the FireBall Item 09/09/2025

--- src/main/java/net/rem/boot/BootMod.java
+++ src/main/java/net/rem/boot/BootMod.java
@@ -32,7 +32,7 @@ import net.rem.boot.item.FireBallItem;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(net.rem.boot.BootMod.MOD_ID)
+@Mod(BootMod.MOD_ID)
 public class BootMod
 {
 
--- src/main/java/net/rem/boot/entity/FireBallEntity.java
+++ src/main/java/net/rem/boot/entity/FireBallEntity.java
@@ -54,7 +54,7 @@ public class FireBallEntity extends ThrowableItemProjectile {
         super.onHitEntity(result);
         if (!this.level().isClientSide) {
             Entity entity = result.getEntity();
-            entity.hurt(this.damageSources().thrown(this, this.getOwner()), 3000.0F);
+            entity.hurt(this.damageSources().thrown(this, this.getOwner()), 5.0f);
             //this.level().explode(this, entity.getX(), entity.getY(), entity.getZ(), 0.5F, Level.ExplosionInteraction.TNT);
         }
     }
