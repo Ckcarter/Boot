77517cdf08cf6c681bcbf1ee0391410a15c67dca
Che Carter <checarter80@gmail.com>
2025-09-09 13:13:35 -0500

    Testing the FireBall Item 09/09/2025

---
+++ src/main/java/net/rem/boot/DeathMessages.java
@@ -0,0 +1,18 @@
+package net.rem.boot;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = BootMod.MOD_ID)
+public class DeathMessages {
+    @SubscribeEvent
+    public static void onDeath(LivingDeathEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            Component message = Component.translatable("death.attack.bootmod.boot", player.getDisplayName());
+            event.setDeathMessage(message);
+        }
+    }
+}
